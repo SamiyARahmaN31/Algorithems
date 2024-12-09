2b7eb597f9c5dc690704f6cb4b12a0ca056b9d74
Samiya Rahman <samiyarahman50020@gmail.com>
2024-12-10 01:29:30 +0600

    Add files via upload

---
+++ BUBBLE sort.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    // Traverse through all elements in the array
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already sorted
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+    cout << "Sorted array is: ";
+    printArray(arr, n);
+
+    return 0;
+}
